42b638d86c41c0aa77b3214b247296446175bc69
bartmant <antczak.bar@gmail.com>
2018-10-15 11:41:17 +0200

    Mapowanie skladnikow, panel dodawania skladniow - BuildControls

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,27 @@ import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 import classes from './Burger.css';
 
 const burger = (props) => {
+
+    // Zlozone mapowanie w celu wygenerowania okreslonej ilosc obiektow
+    let transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient key={igKey + i} type={igKey} />;
+            });
+        })
+        // Przeksztalcenie w puste array TODO: Poczytac o map i reduce
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Zacznij wprowadzac skladniki!</p>
+    }
+
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bred-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {transformedIngredients}
             <BurgerIngredient type="bred-bottom" />
         </div>
     );
