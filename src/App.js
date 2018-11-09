41e0f96f0ab14698050a83bb9e2b4ed337cd53a5
bartmant <antczak.bar@gmail.com>
2018-11-09 09:46:22 +0100

    Dodanie checkout container i rozpoczecie pracy nad routingiem

--- src/App.js
+++ src/App.js
@@ -2,17 +2,26 @@ import React, { Component } from 'react';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <Layout>
-          <BurgerBuilder/>
-        </Layout>
-      </div>
-    );
-  }
+
+    componentDidMount () {
+        setTimeout(() => {
+            this.setState({show: false})
+        }, 5000)
+    }
+
+    render() {
+        return (
+          <div>
+            <Layout>
+                <BurgerBuilder/>
+                <Checkout/>
+            </Layout>
+          </div>
+        );
+    }
 }
 
 export default App;
